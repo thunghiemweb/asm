77f8485bf78603e7a54d0c43f7f83bbccfdb0fd3
tin funix <tinnbFX16153@funix.edu.vn>
2022-07-08 00:39:06 +0700

    commit dau tien

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { STAFFS } from './shared/staffs';
+import StaffList from './StaffListComponent';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { dsnv: STAFFS };
+  }
+
 
   render() {
     return (
-      <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+      <div>
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand>
+              Ứng dụng quản lý nhân sự v1.0
+            </NavbarBrand>
+          </div>
+        </Navbar>
+
+        <StaffList dsnv={this.state.dsnv} />
+
       </div>
     );
   }
 }
-
+//
 export default App;
---
+++ src/StaffListComponent.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import {
+    Card, CardBody, CardText,
+    CardTitle
+} from 'reactstrap';
+
+
+import dateFormat from "dateformat";
+
+
+class StaffList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedNV: null
+        }
+    }
+
+    renderChonNV(dish) {
+        this.setState({ selectedNV: dish });
+    }
+
+    renderNV(nv) {
+        if (nv != null)
+            return (
+                <Card>
+                    <CardBody>
+                        <CardTitle>{nv.name}</CardTitle>
+                        <CardText>Ngày sinh: {dateFormat(nv.doB, "dd/mm/yyyy")}</CardText>
+                        <CardText>Ngày vào công ty: {dateFormat(nv.startDate, "dd/mm/yyyy")}</CardText>
+                        <CardText>Phòng ban: {nv.department.name}</CardText>
+                        <CardText>Số ngày nghỉ còn lại: {nv.annualLeave} ngày</CardText>
+                        <CardText>Số ngày đã đi làm thêm: {nv.overTime} ngày</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return (
+                <div>
+                    <p>Bấm vào tên nhân viên để xem thông tin.</p>
+                </div>
+            );
+    }
+
+    render() {
+
+        const menu = this.props.dsnv.map((e) => {
+            return (
+                <div className="col-sx-12 col-sm-6 col-md-4">
+                    <Card key={e.id} onClick={() => this.renderChonNV(e)}>
+                        <CardTitle>{e.name}</CardTitle>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+
+                <div className="row">
+                    <div className="col-sx-12 col-sm-12 col-md-12">
+                        {this.renderNV(this.state.selectedNV)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default StaffList;
